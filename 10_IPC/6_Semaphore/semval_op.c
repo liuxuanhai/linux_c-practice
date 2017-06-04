f4dffd39ae352c3b600019c1964f137b0f08bd0f
grb <473602668@qq.com>
2017-06-04 05:52:58 -0700

    add file 6_semphore/creatsem.c sem_P_V.c semval_op.c

---
+++ 10_IPC/6_Semaphore/semval_op.c
@@ -0,0 +1,31 @@
+/*================================================================
+*   Copyright (C) 2017 free for learn.
+*   
+*   file:			semval_op.c
+*   breif:			获取和设置单个信号量的值 ---semctl()	
+*   history：		2017-06-04renbin.guo created
+*   note：		
+*   usage：		
+*
+================================================================*/
+
+int semval_op(int semid, int index, int cmd)
+{
+		if(index < 0)
+				error_handler("index invaid\n");
+
+		/* 或者和设置单个信号 */
+		if(cmd == GETVAL || cmd == SETVAL)
+
+				/* 		int semctl(int semid, int semnum, int cmd, ...);	
+				 * 		可见参数是可变的,依赖于第三个参数cmd
+				 *
+				 *
+				 * */
+				
+				return semctl(semid,index,cmd,0);
+
+		printf("cannot surport cmd :%d\n",cmd);
+		return -1;
+}
+
